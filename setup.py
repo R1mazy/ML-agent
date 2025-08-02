ef8f178c6b730d5b0488172705ad4e827b88869f
Roman Lebedev <r.leb@inbox.ru>
2025-08-02 18:02:09 +0800

    add req

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_bot",
+    version="0.1.0",
+    author="Lebedev Roman",
+    author_email="r.leb@inbox.ru",
+    packages=find_packages(),
+    install_requires=[]
+)
